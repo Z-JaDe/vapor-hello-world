3e9d7b327e539c4445bee887b974f1c10760408f
郑军铎 <zjade@outlook.com>
2018-09-18 11:06:40 +0800

    小修改

--- Sources/App/boot.swift
+++ Sources/App/boot.swift
@@ -2,5 +2,10 @@ import Vapor
 
 /// Called after your application has initialized.
 public func boot(_ app: Application) throws {
-    // your code here
+    let client = try app.make(Client.self)
+    
+    let time = Date().timeIntervalSince1970
+    let res = try client.get("http://vapor.codes").wait()
+    print(res)
+    print(Date().timeIntervalSince1970 - time)
 }
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,18 +1,33 @@
 import Vapor
 
+struct User: Content {
+    var bool:Bool = true
+    var int:Int = 12
+    var int8:Int8 = 2
+    var double:Double = 6.1
+    var float:Float = 3.1
+    var name: String = "Vapor User"
+    var email: String = "user@vapor.codes"
+}
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
     // Basic "Hello, world!" example
-    router.get("hello") { req in
+    router.get("_hello") { req in
         return "Hello, world!"
     }
-    router.group("test") { (router) in
-        router.get("ads",PathComponent.parameter("name")) { (req) -> String in
-            guard let name = req.parameters.values.first?.value else {
-                throw Abort.redirect(to: "https://vapor.codes")
-            }
-            return name
+    router.get("hello") { req -> Future<String> in
+        let promise = req.eventLoop.newPromise(Void.self)
+        DispatchQueue.global().async {
+            sleep(5)
+            promise.succeed()
         }
+        return promise.futureResult.transform(to: "Hello, world!")
+    }
+    router.group("test") { (router) in
+        router.get("ads") { (req) -> User in
+            return User()
+        }
+        
     }
     
     // Example of configuring a controller
